6ae090e992af0154e22a8bd1c6ad675f14da531f
sergiorozada12 <sergiorozada12@gmail.com>
2023-09-14 12:09:30 +0200

    feature: environments added

--- src/environments.py
+++ src/environments.py
@@ -1,46 +1,106 @@
+from typing import List, Tuple
+
 import numpy as np
+import gym
 
-# SETUP DESCRIPTION
-# - Wireless communication setup, focus in one user sharing the media with other users
-# - Finite horizon transmission (T slots)
-# - User is equipped with a battery and queue where info is stored
-# - K orthogonal channels, user can select power for each time instant
-# - Rate given by Shannon's capacity formula
 
-# STATES
-# - Amount of energy in the battery: bt (real and positive)
-# - Amount of packets in the queue: queuet (real and positive)
-# - Normalized SNR (aka channel gain) for each of the channels: gkt (real and positive)
-# - Channel being currently occupied: ok (binary)
+class GridWorldEnv:
+    def __init__(
+            self,
+            nS: int=25,
+            nA: int=4,
+            W: int=5,
+            H: int=5,
+    ) -> None:
+        self.nS = nS
+        self.nA = nA
+        self.W = W
+        self.H = H
 
-# ACTIONS
-# - Accessing or not each of the K channels pkt
-# - Tx power for each of the K channels
-# - We can merge both (by setting pkt=0)
+        incs = {
+            0: (0, -1), # UP
+            1: (-1, 0), # LEFT
+            2: (0, +1), # DOWN
+            3: (+1, 0)  # RIGHT
+        }
+
+        P = np.zeros((nS, nS, nA))
+        R = np.zeros((nS, nS, nA))
+        P_0 = np.ones(nS) / (nS - 2)
+        P_0[0] = 0
+        P_0[nS - 1] = 0
+
+        for s in range(nS):
+            for a in range(nA):
+                x, y = s % W, s // W
+                dx, dy = incs[a]
+                x = np.clip(x + dx, 0, W - 1)
+                y = np.clip(y + dy, 0, W - 1)
+
+                sp = y*W + x
+
+                # Transition
+                if s != 0 and s != nS - 1:
+                    P[sp, s, a] = 1
+
+                # Reward
+                if sp == 0 and s != 0:
+                    R[sp, s, a] = 50
+                elif sp == nS - 1 and s != nS - 1:
+                    R[sp, s, a] = 100
+                elif s == 0 or s == nS - 1:
+                    R[:, s, a] = 0
+        self.env = gym.make('matrix_mdp/MatrixMDP-v0', p_0=P_0, p=P, r=R)
+
+    def reset(self):
+        return self.env.reset()
+
+    def step(self, a):
+        return self.env.step(a)
+
 
 
 class WirelessCommunicationsEnv:
+    """
+    SETUP DESCRIPTION
+    - Wireless communication setup, focus in one user sharing the media with other users
+    - Finite horizon transmission (T slots)
+    - User is equipped with a battery and queue where info is stored
+    - K orthogonal channels, user can select power for each time instant
+    - Rate given by Shannon's capacity formula
+
+    STATES
+    - Amount of energy in the battery: bt (real and positive)
+    - Amount of packets in the queue: queuet (real and positive)
+    - Normalized SNR (aka channel gain) for each of the channels: gkt (real and positive)
+    - Channel being currently occupied: ok (binary)
+
+    ACTIONS
+    - Accessing or not each of the K channels pkt
+    - Tx power for each of the K channels
+    - We can merge both (by setting pkt=0)
+    """
     def __init__(
         self,
-        T=10,                         # Number of time slots
-        K=3,                          # Number of channels
-        snr_max=10,                   # Max SNR
-        snr_min=2,                    # Min SNR
-        snr_autocorr=0.7,             # Autocorrelation coefficient of SNR
-        P_occ=np.array([              # Prob. of transition of occupancy
+        T: int=10,                         # Number of time slots
+        K: int=3,                          # Number of channels
+        snr_max: float=10,                 # Max SNR
+        snr_min: float=2,                  # Min SNR
+        snr_autocorr: float=0.7,           # Autocorrelation coefficient of SNR
+        P_occ: np.ndarray=np.array([       # Prob. of transition of occupancy
             [0.3, 0.5],
             [0.7, 0.5],
         ]),
-        occ_initial=[1, 1, 1],        # Initial occupancy state 
-        batt_harvest=3,               # Battery to harvest following a Bernoulli
-        P_harvest=0.5,                # Probability of harvest energy
-        batt_initial=5,               # Initial battery
-        batt_max_capacity=50,         # Maximum capacity of the battery
-        batt_weight=1.0,              # Weight for the reward function
-        queue_initial=20,             # Initial size of the queue
-        queue_weight=1e-1,            # Weight for the reward function
-        loss_busy=0.80,               # Loss in the channel when busy
-    ):
+        occ_initial: List[int]=[1, 1, 1],  # Initial occupancy state 
+        batt_harvest: float=3,             # Battery to harvest following a Bernoulli
+        P_harvest: float=0.5,              # Probability of harvest energy
+        batt_initial: float=5,             # Initial battery
+        batt_max_capacity: float=50,       # Maximum capacity of the battery
+        batt_weight: float=1.0,            # Weight for the reward function
+        queue_initial: float=20,           # Initial size of the queue
+        queue_weight: float=1e-1,          # Weight for the reward function
+        loss_busy: float=0.80,             # Loss in the channel when busy
+    ) -> None:
         self.T = T
         self.K = K
 
@@ -61,7 +121,7 @@ class WirelessCommunicationsEnv:
 
         self.loss_busy = loss_busy
 
-    def step(self, p):
+    def step(self, p: np.ndarray):
         if np.sum(p) > self.batt[self.t]:
             p = self.batt[self.t] * p / np.sum(p)
 
@@ -87,9 +147,6 @@ class WirelessCommunicationsEnv:
             packets = np.sum(self.c[:, self.t - 1])
         self.queue[self.t] = self.queue[self.t - 1] - packets
         self.queue[self.t] = max(0, self.queue[self.t])
-
-        # r = self.batt_weight * np.log(1 + self.batt[self.t]) - self.queue_weight * self.queue[self.t]
-        # done = True if self.t == self.T else False
 
         r = 0
         done = False
